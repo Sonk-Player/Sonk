5c1d9f51d7e1b05b0c1afe151a3b2eb9c6b679e5
axie10 <cryptosushi21@gmail.com>
2024-05-23 17:22:51 +0200

    cardalbum, genrecard, buttoncard

--- src/app/routes/pages/home-player-page/home-player-page.component.ts
+++ src/app/routes/pages/home-player-page/home-player-page.component.ts
@@ -1,14 +1,36 @@
 import { Component } from '@angular/core';
 import { PlayerComponent } from '../../../shared/components/player/player.component';
 import { RouterModule } from '@angular/router';
+import { SongBoxComponent } from '../../../shared/components/song-box/song-box.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { GenreCardComponent } from '../../../shared/components/genre-card/genre-card.component';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { AlbumBoxComponent } from '../../../shared/components/album-box/album-box.component';
+
+
 
 @Component({
   selector: 'homePlayerPage',
   standalone: true,
-  imports: [PlayerComponent, RouterModule],
+  imports: [PlayerComponent, RouterModule, SongBoxComponent, MatSidenavModule, GenreCardComponent, ButtonComponent, AlbumBoxComponent],
   templateUrl: './home-player-page.component.html',
   styleUrl: './home-player-page.component.scss'
 })
 export class HomePlayerPageComponent {
+
+  public songtitle = '../../../../assets/img/extremoduro.jpg';
+  public songalbum = 'Extremoduro';
+
+  public albumtitle = '../../../../assets/img/extremoduro.jpg';
+  public albumalbum = 'Extremoduro';
+
+  public image = "https://www.musicinminnesota.com/wp-content/uploads/2022/08/Jordana_MIM-12.jpg";
+  public icon = "https://www.svgrepo.com/show/9441/guitar.svg";
+  public genre = "Rock";
+
+  public text: string = 'Sin nombre';
+ 
+  public route: string = '';
+
 
 }
--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import {MatChipsModule} from '@angular/material/chips';
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [MatChipsModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
-
+ 
+  @Input()
+  text?: string = 'Sin nombre';
+ 
+  @Input()
+  route?: string;
+ 
 }
