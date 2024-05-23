75279da3b8fcb5c5f854ec38a5e5ac667dcd22a3
Jose Moreno <jmrc1505@gmail.com>
2024-05-23 13:40:51 +0200

    Scaffolding

---
+++ src/app/shared/components/song-box/song-box.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-song-box',
+  standalone: true,
+  imports: [],
+  templateUrl: './song-box.component.html',
+  styleUrl: './song-box.component.scss'
+})
+export class SongBoxComponent {
+
+}
