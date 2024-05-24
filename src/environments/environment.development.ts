c0f5e321ab034321886537e11be3c317cdbd1856
Sergio Sanz <sergiosanz2004@gmail.com>
2024-05-24 16:12:42 +0200

    cheaps y evironment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,6 @@ export const environment = {
     production: false,
 
     YT_API : "https://music.youtube.com/",
-    API_BASE_URL_YT : "https://sonkback-production.up.railway.app/api"
+    API_BASE_URL_YT : "http://127.0.0.1:8000/api"
+    // API_BASE_URL_YT : "https://sonkback-production.up.railway.app/api"
 };
