45ddb7f55ab1a7695866bcdc4d21ffd82d7f8541
Jose Moreno <jmrc1505@gmail.com>
2024-05-24 12:02:43 +0200

    artist page

---
+++ src/app/shared/components/song-bar/song-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-song-bar',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './song-bar.component.html',
+  styleUrl: './song-bar.component.scss'
+})
+export class SongBarComponent {
+
+}
