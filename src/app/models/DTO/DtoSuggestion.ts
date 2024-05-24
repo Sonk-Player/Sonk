3590e072752a93dc7debb0364af4eba32f64c109
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-24 10:43:50 +0200

    feat: Add tailwindcss-animated plugin and update package.json

---
+++ src/app/models/DTO/DtoSuggestion.ts
@@ -0,0 +1,37 @@
+export interface DtoSuggestion {
+  category: string;
+  resultType: string;
+  title: string;
+  album: Album;
+  inLibrary: boolean;
+  feedbackTokens: FeedbackTokens;
+  videoId: string;
+  videoType: string;
+  duration: string;
+  year: any;
+  artists: Artist[];
+  duration_seconds: number;
+  isExplicit: boolean;
+  thumbnails: Thumbnail[];
+}
+
+export interface Album {
+  name: string;
+  id: string;
+}
+
+export interface FeedbackTokens {
+  add: any;
+  remove: any;
+}
+
+export interface Artist {
+  name: string;
+  id: string;
+}
+
+export interface Thumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
--- src/app/shared/components/queueSong/queueSong.component.ts
+++ src/app/shared/components/queueSong/queueSong.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Artist, Thumbnail } from '../../../models/DTO/DtoSearch';
 
 @Component({
   selector: 'queueSong',
@@ -10,12 +11,26 @@ import { Component, Input, OnInit } from '@angular/core';
 export class QueueSongComponent implements OnInit {
 
   @Input() songName : string = 'Sin título'
-  @Input() artistName : string = 'Sin artista'
-  @Input() imgCover : string = '../../../../assets/img/noSong.png'
+  @Input() artistName : Artist[] = [{name: 'Sin artista'}]
+  @Input() imgCover : Thumbnail[] =  []
 
+  converPosition : number =-1
+  actualCover : string = '../../../../assets/img/noSong.png'
   constructor() { }
 
   ngOnInit() {
+    this.getConver();
+  }
+
+  getConver(){
+    
+    
+   return this.imgCover[0].url|| '../../../../assets/img/noSong.png';
+  }
+
+
+  getArtistName(){
+    return this.artistName.map((artist) => artist.name).join(', ');
   }
 
 }
