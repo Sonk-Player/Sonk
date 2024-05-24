da3fcb332ba6c62c82718d6b06bfcd97d6c3ccdd
Jose Moreno <jmrc1505@gmail.com>
2024-05-24 12:08:50 +0200

    Squashed commit of the following:
    commit 2325cf1cf6b9a94d8aafc675a1e40ecc97ec961e
    Author: Unai González <unaiitxuu@gmail.com>
    Date:   Fri May 24 09:06:19 2024 +0200
    
        Navbar and Fixes

---
+++ src/app/shared/navbar-player/navbar-player.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-player',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-player.component.html',
+  styleUrl: './navbar-player.component.scss'
+})
+export class NavbarPlayerComponent {
+
+}
