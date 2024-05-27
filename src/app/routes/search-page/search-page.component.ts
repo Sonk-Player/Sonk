e94ed4f91b869acd2050a4982b01d0ac7ec35908
Sergio Sanz García <sergiosanz2004@gmail.com>
2024-05-27 12:41:27 +0200

    a

--- src/app/routes/search-page/search-page.component.ts
+++ src/app/routes/search-page/search-page.component.ts
@@ -7,11 +7,17 @@ import { DTOsearch } from '../../models/DTO/DtoSearch';
 import { CommonModule } from '@angular/common';
 import { SongBoxComponent } from '../../shared/components/song-box/song-box.component';
 import { ArtistCardComponent } from '../../shared/components/artist-card/artist-card.component';
+import { SongLargeComponent } from '../../shared/components/song-large/song-large.component';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [CommonModule, SongBoxComponent, ArtistCardComponent],
+  imports: [
+    CommonModule,
+    SongBoxComponent,
+    ArtistCardComponent,
+    SongLargeComponent
+  ],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss'
 })
