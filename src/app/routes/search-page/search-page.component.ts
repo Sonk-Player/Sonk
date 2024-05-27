bafecbf4754f8ab74d1c8375f18b04e49e37c051
AngelAcedo12 <117735273+AngelAcedo12@users.noreply.github.com>
2024-05-27 12:37:22 +0200

    song large

--- src/app/routes/search-page/search-page.component.ts
+++ src/app/routes/search-page/search-page.component.ts
@@ -6,11 +6,12 @@ import { typesResultSearch } from '../../utils/typesResultSearch';
 import { DTOsearch } from '../../models/DTO/DtoSearch';
 import { CommonModule } from '@angular/common';
 import { SongBoxComponent } from '../../shared/components/song-box/song-box.component';
+import { SongLargeComponent } from '../../shared/components/song-large/song-large.component';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [CommonModule, SongBoxComponent],
+  imports: [CommonModule, SongBoxComponent, SongLargeComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss'
 })
