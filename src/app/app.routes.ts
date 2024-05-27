ad090ba42b437de1ef866de6c5fcae53d09a8820
Sergio Sanz <sergiosanz2004@gmail.com>
2024-05-27 20:10:33 +0200

    delete route artist

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,9 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
 
     {path: '', loadComponent: () => import('./routes/main-page/main-page.component').then(m => m.MainPageComponent)},
-    {path: 'artist', loadComponent: () => import('./routes/artist/artist.component').then(m => m.ArtistComponent)},
     {path: 'artist/:id', loadComponent: () => import('./routes/artist/artist.component').then(m => m.ArtistComponent)},
     {path: 'search/:name', loadComponent: () => import('./routes/search-page/search-page.component').then(m => m.SearchPageComponent)},
+    {path: '**', redirectTo: '', pathMatch: 'full'},
 
 
 ];
