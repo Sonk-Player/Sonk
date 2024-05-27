34028d1caaa7da9790a284cc8181960f26b99e33
Sergio Sanz García <sergiosanz2004@gmail.com>
2024-05-27 12:03:15 +0200

    search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ export const routes: Routes = [
 
     {path: '', loadComponent: () => import('./routes/main-page/main-page.component').then(m => m.MainPageComponent)},
     {path: 'artist', loadComponent: () => import('./routes/artist/artist.component').then(m => m.ArtistComponent)},
-
+    {path: 'search/:name', loadComponent: () => import('./routes/search-page/search-page.component').then(m => m.SearchPageComponent)},
 
 
 ];
--- src/app/routes/search-page/search-page.component.ts
+++ src/app/routes/search-page/search-page.component.ts
@@ -1,22 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { YtApiServiceService } from '../../services/ytApi-service.service';
 import { ActivatedRoute } from '@angular/router';
+import ytService from 'youtube-player';
+import { typesResultSearch } from '../../utils/typesResultSearch';
+import { DTOsearch } from '../../models/DTO/DtoSearch';
+import { CommonModule } from '@angular/common';
+import { SongBoxComponent } from '../../shared/components/song-box/song-box.component';
 
 @Component({
   selector: 'app-search-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, SongBoxComponent],
   templateUrl: './search-page.component.html',
   styleUrl: './search-page.component.scss'
 })
-export class SearchPageComponent {
+export class SearchPageComponent implements OnInit {
 
-  constructor(private ytService:YtApiServiceService, private activeRouter:ActivatedRoute ) { 
+  private activatedRoute = inject(ActivatedRoute);
+  private ytService = inject(YtApiServiceService);
+
+  public search = signal<string>('');
+  suggestionMap: Map<string, DTOsearch[]> = new Map(
+    [
+      ['songs', []],
+      ['albums', []],
+      ['artists', []],
+      ['playlist', []],
+      // ['topResult', []],
+      ['episodes', []],
+      ['posdcast', []],
+
+    ]
+
+  );
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe((params: any) => {
+      let { name } = params;
+
+      this.ytService.search(name).subscribe((res) => {
+        res.map((item) => {
+          if (item.category == new typesResultSearch().songs) {
+            this.suggestionMap.get('songs')?.push(item)
+          }
+          if (item.category == new typesResultSearch().albums) {
+            this.suggestionMap.get('albums')?.push(item)
+          }
+          if (item.category == new typesResultSearch().artists) {
+            this.suggestionMap.get('artists')?.push(item)
+          }
+          if (item.category == new typesResultSearch().playlist) {
+            this.suggestionMap.get('playlist')?.push(item)
+          }
+          // if (item.category == new typesResultSearch().topResult) {
+          //   this.suggestionMap.get('topResult')?.push(item)
+          // }
+
+        })
+        console.log(this.suggestionMap)
+        console.log(this.suggestionMap.get('songs'));
+
+      }
+      );
+    });
 
   }
-
-
-
-  
-
 }
