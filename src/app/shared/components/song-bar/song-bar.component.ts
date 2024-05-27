691501ddfe4888e300ac8c03065af1668b884499
axie10 <cryptosushi21@gmail.com>
2024-05-27 18:48:35 +0200

    play del top 5 de template artist

--- src/app/shared/components/song-bar/song-bar.component.ts
+++ src/app/shared/components/song-bar/song-bar.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { PlayerServiceService } from '../../../services/player-service.service';
+import { DTOsearch } from '../../../models/DTO/DtoSearch';
+import { YtApiServiceService } from '../../../services/ytApi-service.service';
 
 @Component({
   selector: 'app-song-bar',
@@ -10,7 +13,20 @@ import { MatIconModule } from '@angular/material/icon';
 })
 export class SongBarComponent {
 
+  private ytService = inject(YtApiServiceService);
+  private playerService = inject(PlayerServiceService);
+  @Input() song : string | undefined;
+
   @Input()
   public title: string = '';
+
+  play(){
+    if(this.playerService.actualSong != undefined){
+      this.ytService.getSong(this.song).subscribe((song) => {
+        this.playerService.setSong(song);
+        this.playerService.playSong();
+      })
+    }
+  }
 
 }
