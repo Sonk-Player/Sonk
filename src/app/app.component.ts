a75453569d6e75e3b278fb294d62af984b4b326a
Unai González <unaiitxuu@gmail.com>
2024-05-29 16:48:56 +0200

    Fix Cookies Consent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -65,49 +65,37 @@ export class AppComponent {
    constructor(private ccService: NgcCookieConsentService){}
 
    ngOnInit() {
-     // subscribe to cookieconsent observables to react to main events
      this.popupOpenSubscription = this.ccService.popupOpen$.subscribe(
        () => {
-         // you can use this.ccService.getConfig() to do stuff...
+
        });
 
      this.popupCloseSubscription = this.ccService.popupClose$.subscribe(
        () => {
-         // you can use this.ccService.getConfig() to do stuff...
        });
 
      this.initializingSubscription = this.ccService.initializing$.subscribe(
        (event: NgcInitializingEvent) => {
-         // the cookieconsent is initilializing... Not yet safe to call methods like `NgcCookieConsentService.hasAnswered()`
-         console.log(`initializing: ${JSON.stringify(event)}`);
        });
 
      this.initializedSubscription = this.ccService.initialized$.subscribe(
        () => {
-         // the cookieconsent has been successfully initialized.
-         // It's now safe to use methods on NgcCookieConsentService that require it, like `hasAnswered()` for eg...
-         console.log(`initialized: ${JSON.stringify(event)}`);
        });
 
      this.initializationErrorSubscription = this.ccService.initializationError$.subscribe(
        (event: NgcInitializationErrorEvent) => {
-         // the cookieconsent has failed to initialize...
-         console.log(`initializationError: ${JSON.stringify(event.error?.message)}`);
        });
 
      this.statusChangeSubscription = this.ccService.statusChange$.subscribe(
        (event: NgcStatusChangeEvent) => {
-         // you can use this.ccService.getConfig() to do stuff...
        });
 
      this.revokeChoiceSubscription = this.ccService.revokeChoice$.subscribe(
        () => {
-         // you can use this.ccService.getConfig() to do stuff...
        });
 
        this.noCookieLawSubscription = this.ccService.noCookieLaw$.subscribe(
        (event: NgcNoCookieLawEvent) => {
-         // you can use this.ccService.getConfig() to do stuff...
        });
    }
 
