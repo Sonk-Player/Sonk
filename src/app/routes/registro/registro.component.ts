2b3d3b0ce4c5f58a274d14e934668b0cc6734222
Jose Moreno <jmrc1505@gmail.com>
2024-05-29 09:25:16 +0200

    form validation

--- src/app/routes/registro/registro.component.ts
+++ src/app/routes/registro/registro.component.ts
@@ -1,18 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../../auth/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registro',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './registro.component.html',
   styleUrls: ['./registro.component.scss']
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private authService: AuthService) { }
 
   ngOnInit() {
   }
+  
+
+  registerForm = this.fb.group({
+    email: ['', Validators.required, Validators.email],
+    username: ['', Validators.required, Validators.minLength(3), Validators.maxLength(15)],
+    password: ['', Validators.required, Validators.minLength(6), Validators.maxLength(16)],
+    confirmPassword: ['', Validators.required, Validators.minLength(6), Validators.maxLength(16)]
+  },{
+    validators: [
+      this.authService.isFieldOneEqualFieldTwo('password', 'passwordConf'),
+    ]
+  })
 
 }
