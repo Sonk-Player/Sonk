3fc16b3d3e2f9467955ca53c982c29819af20144
Unai González <unaiitxuu@gmail.com>
2024-05-30 09:19:30 +0200

    UX Fixes

--- src/app/routes/layout/auth-layout/auth-layout.component.ts
+++ src/app/routes/layout/auth-layout/auth-layout.component.ts
@@ -12,37 +12,10 @@ import { RouterModule } from '@angular/router';
 
 export class AuthLayoutComponent implements OnInit {
 
-
-  public imagenes = [
-    {
-      url: '../../../../assets/login/login1.jpg',
-    },
-    {
-      url: '../../../../assets/login/login2.jpg',
-    },
-    {
-      url: '../../../../assets/login/login4.jpg',
-    },
-    {
-      url: '../../../../assets/login/login5.jpg',
-    },
-  ];
-
-  image : string = '';
-
-
-  changeBgImage() {
-    setInterval(() => {
-      this.currentImageIndex = (this.currentImageIndex + 1) % this.imagenes.length;
-      this.image = this.imagenes[this.currentImageIndex].url;
-    }, 10000);
-  }
-
   public currentImageIndex = 0;
 
   ngOnInit() {
-    this.image = this.imagenes[this.currentImageIndex].url;
-    this.changeBgImage();
+
   }
 
   constructor() { }
