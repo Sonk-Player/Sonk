8864650b6beb002d1e8cdbe05e3fbe64c24b9b7a
Unai González <unaiitxuu@gmail.com>
2024-06-06 11:36:19 +0200

    Initial upload of mobile comp

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.sonk.app',
+  appName: 'Sonk',
+  webDir: 'dist/sonk/browser',
+};
+
+export default config;
